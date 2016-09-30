c54130648de5ff674ee2395ecfa7e41d8d3931d8
João Teló <joaotelo.nh@hotmail.com>
2016-09-30 09:24:32 -0300

    with ceedling for unit testing

---
+++ src/Sum.c
@@ -0,0 +1,5 @@
+#include "Sum.h"
+
+int Sum(int a, int b){
+    return a+b;
+}
---
+++ test/test_Sum.c
@@ -0,0 +1,9 @@
+#include "unity.h"
+#include "Sum.h"
+
+void setUp(void){}
+void tearDown(void){}
+
+void test_simpleSum(void){
+    TEST_ASSERT_EQUAL_INT(10, Sum(5,6));
+}
